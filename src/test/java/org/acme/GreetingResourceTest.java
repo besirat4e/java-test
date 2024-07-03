891eb98efe1830a8ac666c1cf79ce11da8990471
Besir Kadrioski <besir.kadrioski@fourenergy.de>
2024-07-03 16:27:46 +0200

    fix test

--- src/test/java/org/acme/GreetingResourceTest.java
+++ src/test/java/org/acme/GreetingResourceTest.java
@@ -14,7 +14,7 @@ class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus RESTi"));
+             .body(is("Hello from Quarkus REST"));
     }
 
 }
